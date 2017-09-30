ddffe885e37bc951e6034e52def85fb9c82a1a42
daruiz31 <d4ruiz31@gmail.com>
2017-09-29 21:13:46 -0500

    add interface: 	- ui add class: 	- MonopolyConsole (juego en consola) change class: 	-MonopolyTest

--- src/UI/MonopolyTest.java
+++ src/UI/MonopolyTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package UI;
 
 import Data.Board;
@@ -11,8 +6,11 @@ import java.util.Scanner;
 
 /**
  *
- * @author Juan Ochoa
+ * @author 
+ *  - Juan Ochoa
+ *  - Diego Ruiz
  */
+
 public class MonopolyTest {
 
     /**
